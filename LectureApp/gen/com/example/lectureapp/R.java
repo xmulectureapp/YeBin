a845513ec8f23fe5bbe6ee480fbf0e10d3cc453c
xianyuxmu <xianyuxmu@gmail.com>
2014-07-04 11:31:48 +0800

    2014年7月4日 早上汇总版本
    今天由Yao实现了开机界面及其Fade in效果
    泽扬成功移植WeChat代码

--- LectureApp/gen/com/example/lectureapp/R.java
+++ LectureApp/gen/com/example/lectureapp/R.java
@@ -8,6 +8,20 @@
 package com.example.lectureapp;
 
 public final class R {
+    public static final class anim {
+        public static final int fade_in=0x7f040000;
+        public static final int fade_out=0x7f040001;
+        public static final int head_in=0x7f040002;
+        public static final int head_out=0x7f040003;
+        public static final int hold=0x7f040004;
+        public static final int hold_long=0x7f040005;
+        public static final int push_bottom_in=0x7f040006;
+        public static final int push_bottom_out=0x7f040007;
+        public static final int push_top_in=0x7f040008;
+        public static final int push_top_in2=0x7f040009;
+        public static final int push_top_out=0x7f04000a;
+        public static final int push_top_out2=0x7f04000b;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -18,28 +32,52 @@ public final class R {
          would include 7" and 10" devices in landscape (~960dp and ~1280dp respectively).
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int logo_launcher=0x7f020001;
+        public static final int app_start=0x7f020000;
+        public static final int bottom_bar=0x7f020001;
+        public static final int hotlecturecenter_normal=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
+        public static final int logo_launcher=0x7f020004;
+        public static final int mycenter_normal=0x7f020005;
+        public static final int noticecenter_normal=0x7f020006;
+        public static final int sanpang123=0x7f020007;
+        public static final int submitcenter_normal=0x7f020008;
+        public static final int subscribecenter_pressed=0x7f020009;
     }
     public static final class id {
-        public static final int action_settings=0x7f080001;
-        public static final int container=0x7f080000;
+        public static final int action_settings=0x7f09000a;
+        public static final int container=0x7f090000;
+        public static final int img_hotlecturecenter=0x7f090005;
+        public static final int img_mycenter=0x7f090008;
+        public static final int img_noticecenter=0x7f090006;
+        public static final int img_submitcenter=0x7f090007;
+        public static final int img_subscribecenter=0x7f090004;
+        public static final int img_tab_now=0x7f090003;
+        public static final int main_bottom=0x7f090002;
+        public static final int mainview=0x7f090001;
+        public static final int tabpager=0x7f090009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
-        public static final int fragment_main=0x7f030001;
+        public static final int appstart=0x7f030001;
+        public static final int fragment_main=0x7f030002;
+        public static final int hotlecturecenter=0x7f030003;
+        public static final int mainview=0x7f030004;
+        public static final int mycenter=0x7f030005;
+        public static final int noticecenter=0x7f030006;
+        public static final int submitcenter=0x7f030007;
+        public static final int subscribecenter=0x7f030008;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
+        public static final int action_settings=0x7f060002;
+        public static final int app_name=0x7f060000;
+        public static final int hello_world=0x7f060001;
     }
     public static final class style {
         /** 
@@ -63,10 +101,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- LectureApp/src/com/example/lectureapp/MainActivity.java
+++ LectureApp/src/com/example/lectureapp/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.appstart);
 
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
